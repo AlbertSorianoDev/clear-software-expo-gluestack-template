689b1b288fd30c9534264d0b0bd21ffe9049e323
Alberto Soriano <albertosoriano.e@gmail.com>
2025-05-06 16:34:35 -0500

    feat: added gluestack library and configuration

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,51 @@
 import "@/global.css";
-import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
 
-export default function Layout() {
+import { GluestackUIProvider } from "@/components/gluestack/gluestack-ui-provider";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { useColorScheme } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
+
+export { ErrorBoundary } from "expo-router";
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    OpenSans: require("@assets/fonts/OpenSans-Regular.ttf"),
+    ...FontAwesome.font,
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <LayoutNav />;
+}
+
+function LayoutNav() {
+  const colorScheme = useColorScheme();
+
   return (
-    <>
-      <StatusBar style="auto" />
-      <Stack screenOptions={{ headerShown: false }} />
-    </>
+    <GluestackUIProvider mode={(colorScheme ?? "light") as "light" | "dark"}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
+        <Stack screenOptions={{ headerShown: false }} />
+      </ThemeProvider>
+    </GluestackUIProvider>
   );
 }
