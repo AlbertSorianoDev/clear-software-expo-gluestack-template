52c5c48dbc59d9a8df9c6206f74b94c1921bff21
Alberto Soriano <albertosoriano.e@gmail.com>
2025-07-21 15:24:01 -0500

    refactor: changing font loading and types folders

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,10 @@
 import "@/global.css";
 
-import OpenSansFont from "@assets/fonts/OpenSans-Regular.ttf";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
-import { useColorScheme } from "react-native";
 
 import { GluestackUIProvider } from "@/screens/components/ui/gluestack-ui-provider";
 
@@ -17,14 +13,7 @@ void SplashScreen.preventAutoHideAsync();
 export { ErrorBoundary } from "expo-router";
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    OpenSans: OpenSansFont,
-    ...FontAwesome.font,
-  });
-
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+  const loaded = true;
 
   useEffect(() => {
     if (loaded) {
@@ -40,7 +29,7 @@ export default function RootLayout() {
 }
 
 function LayoutNav() {
-  const colorScheme = useColorScheme();
+  // const colorScheme = useColorScheme();
 
   return (
     //  (colorScheme ?? "light") as "light" | "dark"}
@@ -48,7 +37,7 @@ function LayoutNav() {
       {/* colorScheme === "dark" ? DarkTheme : DefaultTheme */}
       <ThemeProvider value={DefaultTheme}>
         {/* colorScheme === "dark" ? "light" : "dark" */}
-        <StatusBar style={"light"} />
+        <StatusBar style={"dark"} />
         <Stack screenOptions={{ headerShown: false }} />
       </ThemeProvider>
     </GluestackUIProvider>
