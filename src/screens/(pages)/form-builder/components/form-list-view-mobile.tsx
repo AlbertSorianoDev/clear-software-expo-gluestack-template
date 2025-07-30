c8d2acc251273670d9b4460be4eca4a6c23c326a
Alberto Soriano <albertosoriano.e@gmail.com>
2025-07-30 10:56:38 -0500

    fix(form-list-view-mobile): import error

--- src/screens/(pages)/form-builder/components/form-list-view-mobile.tsx
+++ src/screens/(pages)/form-builder/components/form-list-view-mobile.tsx
@@ -9,17 +9,17 @@ import { FormSectionWrapper } from "../edit/components/form-section-wrapper";
 import { RenderPreviewQuestion } from "../edit/components/form-view/render-question-preview";
 import { useEditFormBuilderPageStore } from "../edit/store/edit-form-builder-page-store";
 
+import { InputTypeEnum } from "@/data/forms/types/enums";
 import { Box } from "@/screens/components/ui/box";
 import { Heading } from "@/screens/components/ui/heading";
 import { Text } from "@/screens/components/ui/text";
 import { VStack } from "@/screens/components/ui/vstack";
-import { FormInputTypeEnum } from "@/screens/features/types/form-input-type";
 
 export interface QuestionModel {
   id: number;
   title: string;
   description: string;
-  type: FormInputTypeEnum;
+  type: InputTypeEnum;
 }
 
 const questions: QuestionModel[] = [
@@ -27,37 +27,37 @@ const questions: QuestionModel[] = [
     id: 1,
     title: "What's your name?",
     description: "Please enter your full name as it appears on official documents.",
-    type: FormInputTypeEnum.shortText,
+    type: InputTypeEnum.shortText,
   },
   {
     id: 2,
     title: "Tell us about yourself",
     description: "You can include your background, interests, or anything you'd like to share.",
-    type: FormInputTypeEnum.longText,
+    type: InputTypeEnum.longText,
   },
   {
     id: 3,
     title: "What is your favorite season?",
     description: "Select one option that best represents your favorite time of the year.",
-    type: FormInputTypeEnum.singleChoice,
+    type: InputTypeEnum.singleChoice,
   },
   {
     id: 4,
     title: "Which programming languages do you use?",
     description: "You can select more than one if applicable.",
-    type: FormInputTypeEnum.multipleChoice,
+    type: InputTypeEnum.multipleChoice,
   },
   {
     id: 5,
     title: "How satisfied are you with our service?",
     description: "1 being not satisfied at all, 5 being extremely satisfied.",
-    type: FormInputTypeEnum.linearScale,
+    type: InputTypeEnum.linearScale,
   },
   {
     id: 6,
     title: "Select your country",
     description: "Choose the country where you currently reside.",
-    type: FormInputTypeEnum.dropdown,
+    type: InputTypeEnum.dropdown,
   },
 
   {
@@ -65,19 +65,19 @@ const questions: QuestionModel[] = [
     title: "Upload your document",
     description:
       "Upload a relevant file such as your ID, certificate, or any other required document.",
-    type: FormInputTypeEnum.fileUpload,
+    type: InputTypeEnum.fileUpload,
   },
   {
     id: 8,
     title: "Select your birhtday's date",
     description: "Select your birhtday's date",
-    type: FormInputTypeEnum.date,
+    type: InputTypeEnum.date,
   },
   {
     id: 9,
     title: "Select a time",
     description: "Pick a specific time that fits your availability or schedule.",
-    type: FormInputTypeEnum.time,
+    type: InputTypeEnum.time,
   },
 ];
 export const FormListViewMobile = () => {
